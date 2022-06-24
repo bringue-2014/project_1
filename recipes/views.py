b622dfc6153862609f9ef64a14fd8a53a2246144
Silvio Borges <bringue2014@gmail.com>
2022-06-24 15:58:17 -0300

    usando o import render

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render (request, 'recipes/home.html')
+
+
+def sobre(request):
+    return render (request, 'recipes/sobre.html')
+
+
+def contato(request):
+    return render (request, 'recipes/contato.html')
+
+
